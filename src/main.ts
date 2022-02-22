fad38c213e48520bc2ca800e428d613c0de77ab2
maxisam <maxisam@gmail.com>
2022-02-21 23:59:37 -0600

    fix: get github env only

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import {readFileSync} from 'fs';
 import {inspect} from 'util';
 
 async function run(): Promise<void> {
-  const GITHUB_ENV = Object.entries(process.env).sort((a, b) => a[0].localeCompare(b[0]));
-  core.debug(`Github ENV: ${inspect(GITHUB_ENV)}`);
+  const GITHUB_ENV = Object.entries(process.env)
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .filter(([key]) => key.startsWith('GITHUB'))
+    .map(([key, value]) => `${key}: ${value}`);
+  core.debug(`💥Github ENV VARIABLE: ${inspect(GITHUB_ENV)}`);
   const EVENT_STR = readFileSync(process.env['GITHUB_EVENT_PATH'] as string, 'utf8');
-  core.debug(`Github EVENT: ${inspect(JSON.parse(EVENT_STR))}`);
+  core.debug(`💥Github EVENT: ${inspect(JSON.parse(EVENT_STR))}`);
 }
 
 run();
